31ecd0d784562be6b75ef53a44683fba361be53c
HMZ003-coder <hamzashahid003@gmail.com>
2025-01-01 20:39:10 +0500

    Update QuestGame.c
    Fixed Error Issue
    Added some Comments

--- QuestGame.c
+++ QuestGame.c
@@ -15,96 +15,126 @@ void delay(int seconds)
 void start()
 {
 	printf("\033[1;32mQuest to Candor\033[0m\n");
-	printf("\n\nSelect The Difficulty:\n 1:Easy.\n 2:Medium \n 3:Hard\n Answer:");
+	printf("\n\nSelect The Difficulty:\n 1:Easy.\n 2:Medium \n 3:Hard\n Answer(Enter The Number Of Your Choice):");
 	scanf("%d",&difficulty);
 	system("cls");
-	printf("Select Your Class:\n 1:Warrior.\n 2:Healer \n 3:Mage\n Answer:");
+	printf("Select Your Class:\n 1:Warrior.\n 2:Healer \n 3:Mage\n Answer(Enter The Number Of Your Choice):");
 	scanf("%d",&class);
-	if(difficulty==1)
+	if(difficulty==1)	// Setting Difficulty Level Using If Else For Character Stats DiffiCulty Level 1 Easy
 	{
-		switch(class)
+		switch(class) // Setting Character Class Using Switch Case For Character Stats
 		{
 			case 1:
 			{
 				hp=250;
 				dmg=50;
 				hrecovery=0;
+				break;
 			}
 			case 2:
 			{
 				hp=170;
 				dmg=70;
 				hrecovery=30;
+				break;
 			}
 			case 3:
 			{
 				hp=200;
 				dmg=60;
 				hrecovery=10;
-			}
-		}
-		
-	}
-	else if(difficulty==2)
-	{
-		switch(class)
-		{
-			case 1:
-			{
-				hp=250;
-				dmg=50;
-				hrecovery=0;
-			}
-			case 2:
-			{
-				hp=170;
-				dmg=70;
-				hrecovery=30;
-			}
-			case 3:
-			{
-				hp=200;
-				dmg=60;
-				hrecovery=10;
+				break;
+				
 			}
 			default:
 			{
-				printf("Invalid Choice");
+				clear_screen();		//Calling Clear Screen Function To Clear The Screen
+				error(); 	//Calling Error Function To Display Error
+				start();	//Calling Start Function To Go Back Start Function
 			}
 		}
 		
 	}
-	else if(difficulty==3){
-		switch(class)
+	else if(difficulty==2)	// Setting Difficulty Level Using If Else For Character Stats DiffiCulty Level 2 Medium
+	{
+		switch(class)	// Setting Character Class Using Switch Case For Character Stats
+		{
+			case 1:
+			{
+				hp=250;
+				dmg=50;
+				hrecovery=0;
+				break;
+			}
+			case 2:
+			{
+				hp=170;
+				dmg=70;
+				hrecovery=30;
+				break;
+			}
+			case 3:
+			{
+				hp=200;
+				dmg=60;
+				hrecovery=10;
+				break;
+			}
+			default:
+			{
+				clear_screen();		//Calling Clear Screen Function To Clear The Screen
+				error(); 	//Calling Error Function To Display Error
+				start();	//Calling Start Function To Go Back Start Function
+			}
+		}
+		
+	}
+	else if(difficulty==3)	// Setting Difficulty Level Using If Else For Character Stats DiffiCulty Level 3 Hard
+	{
+		switch(class)	// Setting Character Class Using Switch Case For Character Stats
 		{
 			case 1:
 			{
 				hp=200;
 				dmg=100;
 				hrecovery=0;
+				break;
 			}
 			case 2:
 			{
 				hp=150;
 				dmg=100;
 				hrecovery=20;
+				break;
 			}
 			case 3:
 			{
 				hp=175;
 				dmg=80;
 				hrecovery=10;
+				break;
 			}
 			default:
 			{
-				printf("Invalid Choice");
+				clear_screen();		//Calling Clear Screen Function To Clear The Screen
+				error(); 	//Calling Error Function To Display Error
+				start();	//Calling Start Function To Go Back Start Function
 			}
 		}
 		
 	}
+	else{
+		clear_screen();		//Calling Clear Screen Function To Clear The Screen
+		error(); 	//Calling Error Function To Display Error
+		start();	//Calling Start Function To Go Back Start Function
+	}
 	
 	
 	
+}
+int error(){
+    printf("Error: Invalid input detected.\n");
+    
 }
 
 void level_1 (void)
@@ -118,7 +148,7 @@ int main()
 {
   start();
   printf("\n\n");
-  printf("Your Stats Are : \n hp:%d \n Damage per hit:%d \n Health Recovery %d",hp,dmg,hrecovery);
+  printf("Your Stats Are : \n HP:%d \n Damage per hit:%d \n Health Recovery %d",hp,dmg,hrecovery);
   level_1();
 
   return 0;
